774fb0a5fc503fdee7c525853f0f30c5dc7473f0
beefysalad <jpatrickzephyr@gmail.com>
2026-05-29 21:36:00 +0800

    feat: integrate Axios, Clerk, and React Query with initial API service setup

--- apps/mobile/src/app/_layout.tsx
+++ apps/mobile/src/app/_layout.tsx
@@ -1,17 +1,25 @@
 import '@/global.css';
 
+import { ClerkProvider } from '@clerk/expo';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
 import { useColorScheme } from 'react-native';
 
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
 import AppTabs from '@/components/app-tabs';
 
+const queryClient = new QueryClient();
+
 export default function TabLayout() {
   const colorScheme = useColorScheme();
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
+    <ClerkProvider publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+          <AnimatedSplashOverlay />
+          <AppTabs />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </ClerkProvider>
   );
 }
